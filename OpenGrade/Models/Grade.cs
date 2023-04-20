9cb7b4e7d6804a57be35651fc88e22a7dbfbe305
WillTheDeveloper <willthedeveloper13@gmail.com>
2023-04-20 19:25:40 +0100

    Updated grade model value

--- OpenGrade/Models/Grade.cs
+++ OpenGrade/Models/Grade.cs
@@ -4,7 +4,7 @@
 	{
         public Guid gradeId { get; set; }
         public string Gradee { get; set; }
-        public string Value { get; set; }
+        public int Value { get; set; }
         public int Order { get; set; }
         public Guid courseId { get; set; }
     }
